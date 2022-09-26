bee364b69e7dad505fc732cfd3aeb325654b4907
Hatim Sawai <hatimsawai76@gmail.com>
2022-09-26 22:13:53 +0530

    exp3 sql file
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp3/exp3.sql
+++ Exp3/exp3.sql
@@ -1,3 +1,13 @@
 -- @BLOCK
-
+SELECT * FROM doctor;
 -- @BLOCK
+ALTER TABLE doctor DROP Address;
+-- @BLOCK
+ALTER TABLE doctor ADD Address VARCHAR(50);
+UPDATE doctor SET Address = 'Andheri' WHERE D_id<=5;
+UPDATE doctor SET Address = 'Andheri';
+-- @BLOCK
+SELECT D_id, Dname,Salary FROM doctor WHERE Salary>400000 ORDER BY Salary DESC;
+SELECT P_id,Pname,Age,Address FROM patient WHERE Pname like 'r%' ORDER BY Age DESC;
+-- @BLOCK
+SELECT Pname,Address,Ph_no FROM patient WHERE Address like 'b%' OR Address like '%z';
