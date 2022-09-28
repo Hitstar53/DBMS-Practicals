d38de187ac90bebf1f0833cb8f385fd5b8140cc4
Hatim Sawai <hatimsawai76@gmail.com>
2022-09-28 23:01:05 +0530

    updated exp4
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ Exp4/exp4.sql
@@ -0,0 +1,25 @@
+-- @BLOCK
+UPDATE patient SET D_id=NULL WHERE P_id>=6;
+-- @BLOCK
+SELECT patient.P_id,patient.Pname,patient.Age,doctor.Dname,doctor.Field
+FROM patient
+INNER JOIN doctor
+ON patient.D_id = doctor.D_id;
+-- @BLOCK
+SELECT patient.P_id,patient.Pname,patient.Age,doctor.Dname,doctor.Field
+FROM patient
+LEFT JOIN doctor
+ON patient.D_id = doctor.D_id
+ORDER BY patient.Age;
+-- @BLOCK
+SELECT patient.P_id,patient.Pname,patient.Age,doctor.Dname,doctor.Field
+FROM patient
+RIGHT JOIN doctor
+ON patient.D_id = doctor.D_id
+ORDER BY patient.Age;
+-- @BLOCK
+SELECT patient.P_id,patient.Pname,patient.Age,doctor.Dname,doctor.Field
+FROM patient
+CROSS JOIN doctor;
+-- @BLOCK
+SELECT * FROM patient FULL JOIN doctor;
--- exp4.sql
+++
@@ -1,7 +0,0 @@
--- @BLOCK
-SELECT patient.Pname,patient.Age,doctor.Dname,doctor.Field
-FROM patient
-INNER JOIN doctor
-ON patient.D_id = doctor.D_id;
--- @BLOCK
-UPDATE patient SET D_id=NULL WHERE P_id=6;
